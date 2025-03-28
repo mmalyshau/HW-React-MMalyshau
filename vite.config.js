34717eb422644894c2a1bb976f330ead324be71d
Mikita Malyshau <nmalysev41@gmail.com>
2025-03-28 13:00:28 +0200

    feat: did task 1

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@lib": path.resolve(__dirname, "src/shared/lib"),
+      "@processes": path.resolve(__dirname, "src/processes"),
+      "@ui": path.resolve(__dirname, "src/ui"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+});
