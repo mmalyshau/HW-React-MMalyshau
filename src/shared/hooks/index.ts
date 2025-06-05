532b27fb6c58fe0943d815bd39241c113983a50c
Mikita Malyshau <nmalysev41@gmail.com>
2025-06-05 21:51:47 +0300

    fix: removed unnessary files

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,6 +1,5 @@
 export { default as useForm } from './useForm';
 export { default as useFetch } from './useFetch';
 export { default as useValidation } from './useValidation';
-export {default as useAuth} from './useAuth';
 export { default as useAppDispatch } from './useAppDispatch';
 export { default as useAppSelector } from './useAppSelector';
